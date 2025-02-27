2383a88d41b5bb215e7c876fc87fc739a2b9fc5a
Abdalrahman_Gamal <ghozalabdo456@gmail.com>
2025-02-27 20:11:38 +0200

    Adding Counter Event , Finishing Sections

--- Elzero.js
+++ Elzero.js
@@ -1,28 +1,69 @@
-/************ Latest Events Section ************/
-let inputEmail = document.querySelector(".events input");
+/************ Event Count Down ************/
+let dateEvent = new Date("Dec 31,2025").getTime();
 
-// check if There is input in Session Storage
-if(window.sessionStorage.getItem("email")){
-  inputEmail.value = window.sessionStorage.getItem("email");
+
+let counter = setInterval(() => {
+
+  let dateNow = new Date().getTime();
+  let dateDiff = dateEvent - dateNow;
+
+
+  let days = Math.floor(dateDiff /(1000 * 60 * 60 * 24));
+  let hours = Math.floor(dateDiff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+  let minutes = Math.floor(dateDiff % (1000 * 60 * 60) / (1000 * 60));
+  let seconds = Math.floor(dateDiff % (1000 * 60) / (1000));
+
+  document.querySelector(".days").textContent = days < 10 ? `00-${days}` : days < 100 ? `0-${days}` : days;
+  document.querySelector(".hours").textContent = hours < 10 ? `00-${hours}` : hours < 100 ? `0-${hours}` : hours;
+  document.querySelector(".minutes").textContent = minutes < 10 ? `00-${minutes}` : minutes < 100 ? `0-${minutes}` : minutes;
+  document.querySelector(".seconds").textContent = seconds < 10 ? `00-${seconds}` : seconds < 100 ? `0-${seconds}` : seconds;
+  
+
+},1000)
+
+/************ Check On Forms ************/
+let forms = document.forms;
+forms = Array.from(forms);
+
+forms.forEach((form) => {
+
+  // Get All inputs and text areas inside each form /* Except submit input */
+  let inputFields = form.querySelectorAll("input:not([type='submit']) , textarea");
+
+  checkSessionStorage(inputFields); // Check if there are inputs in session storage
+  setSessionStorage(inputFields); // set values in session storage
+
+  form.addEventListener("submit" , (event) => inputIsEmpty(inputFields,event));
+});
+
+
+function setSessionStorage(inputFields) {
+  inputFields.forEach((input) => {
+    input.addEventListener("input",(ele) => {
+      // Setting Inputs in Session Storage
+      window.sessionStorage.setItem(ele.currentTarget.dataset.name , ele.currentTarget.value);
+    });
+  })
 }
 
-// On input => the input will be stored in the session storage
-inputEmail.addEventListener("input",(e) => {
-  window.sessionStorage.setItem("email",e.currentTarget.value);
-});
+function checkSessionStorage(inputFields){
+  inputFields.forEach((input) => {
+    if(window.sessionStorage.getItem(input.dataset.name)){
+      // if Value in Session Storage => get in the Field
+      input.value = window.sessionStorage.getItem(input.dataset.name);
+    }
+  })
+}
 
-document.forms[0].addEventListener("submit", (e) => {
-  // Check if input field is empty
-  if(inputEmail.value === ""){
-    e.preventDefault();
-  }
-  else {
-    e.preventDefault();
-    // => my Linkedin
-    window.location.assign("https://www.linkedin.com/in/abdalrahman-gamal-0972202a4");
-  }
-});
-
+function inputIsEmpty(inputFields,event){
+  inputFields.forEach((input) => {
+    // Check If any input is empty 
+    if(input.value === ""){
+      // the form won't sent
+      event.preventDefault();
+    }
+  })
+}
 
 /************ Pricing Plans Section ************/
 let plans = document.querySelectorAll(".pricing .plan");
@@ -67,7 +108,7 @@ let notExcuted = true;
 
 window.addEventListener("scroll" , () => {
   // Skills Code
-  if(window.scrollY >= ourSkills.offsetTop){
+  if(window.scrollY >= ourSkills.offsetTop - 500){
     spans.forEach((span) => {
       // Filling Levels
       span.style.width = span.dataset.progress;
